9b4b90a939579233063e95be487adc629fb01eb1
Akshay Patil <akshay.patil9111999@gmail.com>
2021-03-07 14:54:32 +0530

    update

--- src/services/aws.ts
+++ src/services/aws.ts
@@ -11,12 +11,46 @@ export const uploadToAWS = (key: string, body: Buffer) => {
     };
     s3.upload(params, (err, data) => {
       if (err) {
-        console.error(err);
-        reject({
-          message: "Image uploading failed please try uploading later !",
-        });
+        console.error("Image uploading failed");
+        reject(err);
       }
       resolve(data.Location);
     });
   });
+};
+
+export const deleteFromAWS = (key: string) => {
+  const s3 = new S3();
+  const params: S3.DeleteObjectRequest = {
+    Bucket: process.env.AWS_BUCKET_NAME!,
+    Key: key,
+  };
+  return s3.deleteObject(params).promise();
+};
+
+export const deleteFolderFromAWS = async (folder: string) => {
+  const s3 = new S3();
+  const listParams: S3.ListObjectsV2Request = {
+    Bucket: process.env.AWS_BUCKET_NAME!,
+    Prefix: folder,
+  };
+
+  const listedObjects = await s3.listObjectsV2(listParams).promise();
+
+  if (!listedObjects.Contents) return;
+
+  if (listedObjects.Contents.length === 0) return;
+
+  const deleteParams = {
+    Bucket: process.env.AWS_BUCKET_NAME!,
+    Delete: { Objects: [] as any },
+  };
+
+  listedObjects.Contents.forEach((content: any) => {
+    deleteParams.Delete.Objects.push({ Key: content.Key });
+  });
+
+  await s3.deleteObjects(deleteParams).promise();
+
+  if (listedObjects.IsTruncated) await deleteFolderFromAWS(folder);
 };
